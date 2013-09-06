69e7b5cec4d9f57295a420992fde2cb76e759267
aonghusflynn <developer@aonghusflynn.com>
2013-09-07 00:21:19 +0100

    Moved fb stuff to json

---
+++ app/controllers/facebook_users_controller.rb
@@ -0,0 +1,24 @@
+class FacebookUsersController < ApplicationController
+  	def index
+  	end
+
+  	def list
+   	if session[:user_id]
+	      @user = User.find(session[:user_id])
+	      @graph = Koala::Facebook::API.new(@user.oauth_token)
+	      @friends = @graph.get_connections("me", "friends")
+	      @people_list = Array.new
+      		for friend in @friends
+	        	h = Hash.new
+	        	h["photo"] = @graph.get_object(friend['id']+'?fields=picture,name')['picture']['data']['url']
+	        	h["id"] = friend["id"]
+	        	h["name"] = @graph.get_object(friend['id']+'?fields=name')["name"]
+	        	h["likes"] = @graph.get_object(friend['id']+'?fields=likes')["data"]
+	        	@people_list << h
+	    	end
+	 	end
+	 	render :json=>@people_list
+  	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Kindler::Application.routes.draw do
   resources :krindles
-
+  
+  get "facebook_users/list"
+  
   get "main/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
