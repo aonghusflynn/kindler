049379a0b6fc44b6c88876bd96ece13a8b5c505c
aonghusflynn <developer@aonghusflynn.com>
2013-09-16 19:50:27 +0100

    some controller change

--- app/controllers/facebook_users_controller.rb
+++ app/controllers/facebook_users_controller.rb
@@ -8,13 +8,9 @@ class FacebookUsersController < ApplicationController
 	      @graph = Koala::Facebook::API.new(@user.oauth_token)
 	      @friends = @graph.get_connections("me", "friends")
 	      @people_list = Array.new
-	      #@friends_list = Array.new
+
           @people_list = @graph.get_object('me?fields=id,name,friends.fields(id,email,name,picture)')['friends']['data']
-          #@people_list.each do |person|
-          #	h= Hash.new
-          #	h[person['id']]=person
-          #	@friends_list << h
-          #end
+  
 	    	
 	 	end
 	 	render :json=>@people_list
